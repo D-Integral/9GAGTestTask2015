dc31fa07c7d71ebbc42ef225f9f838928f9c86e9
Dmytro Skorokhod <d.integralas@gmail.com>
2015-09-13 01:26:54 +0300

    Segmented Control, Navigation Bar & Status Bar were implemented.

--- 9GAG/ViewController.m
+++ 9GAG/ViewController.m
@@ -2,13 +2,15 @@
 //  ViewController.m
 //  9GAG
 //
-//  Created by Dmytro Skorokhod on 9/13/15.
-//  Copyright (c) 2015 Dima Skorokhod. All rights reserved.
+//  Created by Dmytro Skorokhod on 9/12/15.
+//  Copyright (c) 2015 Dmytro Skorokhod. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 
 @end
 
@@ -16,12 +18,43 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	[self setupNavigationBar];
+	[self highlightSelectedSegment];
 }
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (IBAction)segmentChanged:(id)sender {
+	[self highlightSelectedSegment];
+}
+
+#pragma mark -
+#pragma mark Navigation Bar
+
+- (void)setupNavigationBar {
+	[self makeNavigationBarTranslucent];
+	
+	self.navigationController.navigationBar.topItem.title = NSLocalizedString(@"9GAG", @"9GAG");
+	self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+}
+
+- (void)makeNavigationBarTranslucent {
+	[self.navigationController.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+	self.navigationController.navigationBar.shadowImage = [UIImage new];
+	self.navigationController.navigationBar.translucent = YES;
+}
+
+#pragma mark -
+#pragma mark Segmented Control
+
+- (void)highlightSelectedSegment {
+	for (NSInteger i = 0; i < [self.segmentedControl.subviews count]; i++) {
+		UIColor *color = [[self.segmentedControl.subviews objectAtIndex:i] isSelected] ? [UIColor whiteColor] : [self nonSelectedSegmentColor];
+		[[self.segmentedControl.subviews objectAtIndex:i] setTintColor:color];
+	}
+}
+
+- (UIColor *)nonSelectedSegmentColor {
+	return [UIColor colorWithRed:0.5195 green:0.5195 blue:0.5195 alpha:1.0];
 }
 
 @end
