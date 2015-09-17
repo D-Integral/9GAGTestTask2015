ed5268c90690c8aa24061989550205a3fa6871f3
Dmytro Skorokhod <d.integralas@gmail.com>
2015-09-18 00:36:58 +0300

    Initial tests were implemented.

--- 9GAGTests/_GAGTests.m
+++ 9GAGTests/_GAGTests.m
@@ -9,25 +9,17 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "DataManager.h"
+
 @interface _GAGTests : XCTestCase
 
 @end
 
 @implementation _GAGTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void)testDataManager {
+	DataManager *dataManager = [DataManager sharedManager];
+	XCTAssertNotNil(dataManager.dataCollections);
 }
 
 - (void)testPerformanceExample {
---
+++ 9GAGUITests/_GAGUITests.m
@@ -0,0 +1,29 @@
+//
+//  _GAGUITests.m
+//  9GAGUITests
+//
+//  Created by Dmytro Skorokhod on 9/18/15.
+//  Copyright © 2015 Dima Skorokhod. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _GAGUITests : XCTestCase
+
+@end
+
+@implementation _GAGUITests
+
+- (void)setUp {
+    [super setUp];
+	
+    self.continueAfterFailure = NO;
+    [[[XCUIApplication alloc] init] launch];
+}
+
+- (void)testUserInterface {
+	XCUIApplication *app = [XCUIApplication new];
+	XCTAssertEqualObjects([[[app navigationBars] element] identifier], @"9GAG");
+}
+
+@end
